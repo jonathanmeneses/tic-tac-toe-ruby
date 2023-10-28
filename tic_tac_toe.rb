03869436088327cb303116b70d0ebfbeffcb198d
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-27 23:58:18 -0500

    Set up initial script structure  with approach notes

---
+++ tic_tac_toe.rb
@@ -0,0 +1,15 @@
+# tic-tac-toe
+# implementation strategy
+# 1. Create Board
+# 2. Display Board
+# 3. Add function to
+# 2. Create Win Conditions
+# 3. Create way to add
+#
+#
+
+class TicTacToe
+  def initialize(player)
+
+
+end
